e77b5a62056d1cc96ce5c50e50e51b1164d033ba
Óscar Pérez Tobarra <psp_and_oscar@hotmail.com>
2021-05-25 20:16:59 +0200

    Método CalcularRuta

---
+++ DNSGPS/Ruta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using IronPython;
+using Microsoft.Scripting.Hosting;
+using Microsoft.Scripting;
+using System.Collections.Specialized;
+
+public class Ruta
+    {
+        
+        public void CalcularRuta()
+        {
+            ScriptEngine engine = Python.CreateEngine();
+            dynamic scope = engine.CreateScope();
+            
+        }
+    }
