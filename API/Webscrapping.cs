93b37a46800ac99de895581706f394a9600b70f0
Óscar Pérez Tobarra <psp_and_oscar@hotmail.com>
2021-05-26 11:12:37 +0200

    Weather scraping HTMLAgilityPack

--- API/Webscrapping.cs
+++ API/Webscrapping.cs
@@ -10,30 +10,55 @@ using Microsoft.Scripting.Hosting;
 using Microsoft.Scripting;
 using System.Collections.Specialized;
 using System.IO;
+using HtmlAgilityPack;
+using System.Text;
 
 public static class Webscrapping
+{
+    public static void EjecutarCoordenadas()
     {
-        public static void EjecutarCoordenadas()
-        {
-            string pythonFile="";
-            foreach (string nameFile in Directory.GetFiles(Directory.GetCurrentDirectory()))
-            {
-                if (nameFile.Contains("coords.py"))
-                {
-                    pythonFile = nameFile;
-                }
-            }    
+        var html = @"http://html-agility-pack.net/";
+        HtmlWeb web = new HtmlWeb();
+        var htmlDoc = web.Load(html);
 
-            ScriptRuntime py = Python.CreateRuntime();
-            dynamic pyProgram = py.UseFile(pythonFile);          
-        }
 
-        public static void EjecutarTiempo()
-        {
-            string pathPy = @"..\weather.py";
-            ScriptRuntime py = Python.CreateRuntime();
-            dynamic pyProgram = py.UseFile(pathPy);
-            
-        }
-        
     }
+
+    public static void EjecutarTiempo()
+    {
+        var html = @"http://www.tiempo.com/";
+        HtmlWeb web = new HtmlWeb();
+        web.OverrideEncoding = Encoding.UTF8;
+        var htmlDoc = web.Load(html);
+
+        public List<string> tiempos = new List<string>();
+        var nodes = htmlDoc.DocumentNode.SelectNodes("//abbr[@class='des-mapa']");
+        foreach (HtmlNode node in nodes)
+        {
+            tiempos.Add(node.InnerText);
+        }
+
+        public List<string> provincias = new List<string>();
+        nodes = htmlDoc.DocumentNode.SelectNodes("//dt");
+        foreach (HtmlNode node in nodes)
+        {
+            provincias.Add(node.InnerText);
+        }
+
+        using (TextWriter tw = new StreamWriter("provincias.txt"))
+        {
+            foreach (String provincia in provincias)
+                tw.WriteLine(provincia);
+        }
+
+        /*public matrizTiempos = new string[10,10];
+
+        for (int i=0; i< provincias.Count; i++){
+            for (int j=0; j < 2; j++){
+                if (j==0){ matrizTiempos[i][j] = provincias[i]; }
+                else { matrizTiempos[i][j] = tiempos[i]; }
+            }
+        }*/
+
+    }
+}
